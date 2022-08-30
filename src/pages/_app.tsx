323942d15db2c09e31abf3eb0f30af3f5aec2737
Shriram Balaji <hello@shrirambalaji.dev>
2022-08-31 01:25:42 +0530

    remove public/fonts, add @fontsource/jetbrains-mono

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,10 @@ import "@fontsource/inter/400.css";
 import "@fontsource/inter/500.css";
 import "@fontsource/inter/600.css";
 import "@fontsource/inter/700.css";
-
+import "@fontsource/jetbrains-mono";
+import "@fontsource/jetbrains-mono/600.css";
 import "../styles/globals.css";
+
 import type { AppProps } from "next/app";
 
 function App({ Component, pageProps }: AppProps) {
