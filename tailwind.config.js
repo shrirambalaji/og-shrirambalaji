005b5c2e0703c0d9a60803a97fddc31626aa42b7
Shriram Balaji <hello@shrirambalaji.dev>
2022-08-31 00:10:36 +0530

    use Inter instead of Inter var

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
     },
     extend: {
       fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
         mono: ["JetBrains Mono", ...defaultTheme.fontFamily.mono],
       },
       colors: {
