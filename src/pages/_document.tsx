b909ce3229a1cc368240512dae0bad56885a4b7b
Shriram Balaji <hello@shrirambalaji.dev>
2022-08-31 00:22:59 +0530

    set fontface in _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,52 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `
+              @font-face {
+                font-family: "Inter";
+                font-style: normal;
+                font-weight: normal;
+                src: url("/fonts/Inter-Regular.ttf") format("ttf");
+              }
+
+              @font-face {
+                font-family: "Inter";
+                font-weight: 300;
+                src: url("/fonts/Inter-Light.ttf") format("ttf");
+              }
+
+              @font-face {
+                font-family: "Inter";
+                font-style: normal;
+                font-weight: 500;
+                src: url("/fonts/Inter-SemiBold.ttf") format("ttf");
+              }
+
+              @font-face {
+                font-family: "Inter";
+                font-style: normal;
+                font-weight: bold;
+                src: url("/fonts/Inter-Bold.ttf") format("ttf");
+              }
+
+              @font-face {
+                font-family: "JetBrains Mono";
+                font-style: normal;
+                font-weight: normal;
+                src: url("/fonts/JetBrainsMono-Regular.ttf") format("ttf");
+              }
+
+              @font-face {
+                font-family: "JetBrains Mono";
+                font-style: normal;
+                font-weight: bold;
+                src: url("/fonts/JetBrainsMono-Bold.ttf") format("ttf");
+              }          
+          `,
+          }}
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
